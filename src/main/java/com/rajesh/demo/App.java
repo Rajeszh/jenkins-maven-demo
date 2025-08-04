76fe54c3a735aa3e43f09482af8dab1d29ce107c
Rajeszh <99657456+Rajeszh@users.noreply.github.com>
2025-08-04 13:01:13 +0530

    Update App.java

--- src/main/java/com/rajesh/demo/App.java
+++ src/main/java/com/rajesh/demo/App.java
@@ -3,6 +3,8 @@ package com.rajesh.demo;
 
 public class App {
     public static void main(String[] args) {
+
+        helllo
         System.out.println("Hello from Maven + Jenkins project!");
     }
 }
